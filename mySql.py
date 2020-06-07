17e309c82f11bc966d029848e2e46eccc96f341f
javkhaamgl <javkhlanbayar@khabuko.com>
2020-06-07 16:01:10 +0800

    add table

--- mySql.py
+++ mySql.py
@@ -4,6 +4,7 @@ mydb = mysql.connector.connect(user='root', host='localhost', password='', datab
 
 mycursor = mydb.cursor()
 
+
 def deleteData():
     sql_Delete_query = "DELETE FROM  company;"
     sql_1 = " ALTER TABLE company AUTO_INCREMENT=0;"
@@ -11,6 +12,7 @@ def deleteData():
     mycursor.execute(sql_1)
     mydb.commit()
     print("company Deleted successfully ")
+
 
 def delete1Data():
     sql_Delete_query = "DELETE FROM quarterly_dataPL;"
@@ -20,7 +22,7 @@ def delete1Data():
     mydb.commit()
     print("quarterly_dataPL Deleted successfully ")
 
-    
+
 def delete2Data():
     sql_Delete_query = "DELETE FROM quarterly_dataCF;"
     sql_1 = "ALTER TABLE quarterly_dataCF AUTO_INCREMENT=0;"
@@ -28,7 +30,8 @@ def delete2Data():
     mycursor.execute(sql_1)
     mydb.commit()
     print("quarterly_dataCF Deleted successfully ")
-    
+
+
 def delete3Data():
     sql_Delete_query = "DELETE FROM quarterly_dataBS;"
     sql_1 = "ALTER TABLE quarterly_dataBS AUTO_INCREMENT=0;"
@@ -38,9 +41,9 @@ def delete3Data():
     print("quarterly_dataBS Deleted successfully ")
 
 
-def insertData(company_name, stock_price, symbol, previousDay, priceChange, percentChange):
-    sql = "INSERT INTO company ( company_name, stock_price, symbol,previousDay,priceChange,percentChange) VALUES (%s, %s, %s, %s, %s, %s)"
-    val = [(company_name, stock_price, symbol, previousDay, priceChange, percentChange)]
+def insertData(company_name, market_cap, stock_price, symbol, previousDay, priceChange, percentChange):
+    sql = "INSERT INTO company ( company_name, market_cap, stock_price, symbol,previousDay,priceChange,percentChange) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    val = [(company_name, market_cap, stock_price, symbol, previousDay, priceChange, percentChange)]
     mycursor.executemany(sql, val)
     mydb.commit()
 
@@ -52,32 +55,25 @@ def company_id():
     return id
 
 
-def insertDataPL(period, revenues, COGS, operating_income, operating_profit, net_income, company_id):
-    sql = "INSERT INTO quarterly_dataPL (period, revenues, COGS, operating_income, operating_profit, net_income,company_id) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    val = [(str(period), str(revenues), str(COGS), str(operating_income), str(operating_profit), str(net_income),str(company_id))]
+def insertDataPL(period, revenues, cogs, sga, sales_profit, sales_margin, net_income, Gross_sales_margin_float, company_id):
+    sql = "INSERT INTO quarterly_dataPL (period, revenues, cogs, sga, sales_profit, sales_margin, net_income, Gross_sales_margin_float, company_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = [(str(period), str(revenues), str(cogs), str(sga), str(sales_profit), str(sales_margin), str(net_income), str(Gross_sales_margin_float), str(company_id))]
     mycursor.executemany(sql, val)
     mydb.commit()
 
 
 def insertDataCF(Net_Operating_Cash_Flow, Net_Investing_Cash_Flow, Net_Financing_Cash_Flow, Quarterly_free_cash_flow,
                  company_id):
-    sql = "INSERT INTO quarterly_dataCF (Net_Operating_Cash_Flow, Net_Investing_Cash_Flow, Net_Financing_Cash_Flow, Quarterly_free_cash_flow,company_id) VALUES (%s, %s, %s, %s, %s)"
-    val = [(str(Net_Operating_Cash_Flow), str(Net_Investing_Cash_Flow), str(Quarterly_free_cash_flow),
+    sql = "INSERT INTO quarterly_dataCF (Net_Operating_Cash_Flow, Net_Investing_Cash_Flow, Net_Financing_Cash_Flow, Quarterly_free_cash_flow, company_id) VALUES (%s, %s, %s, %s, %s)"
+    val = [(str(Net_Operating_Cash_Flow), str(Net_Investing_Cash_Flow), str(Net_Financing_Cash_Flow),
             str(Quarterly_free_cash_flow), str(company_id))]
     mycursor.executemany(sql, val)
     mydb.commit()
 
 
-def insertDataBS(total_asset, adequacy_ratio, ROA, ROE, tot_shareholder_equity, company_id):
-    sql = "INSERT INTO quarterly_dataBS (total_asset, adequacy_ratio, ROA, ROE,tot_shareholder_equity,company_id) VALUES (%s, %s, %s, %s, %s, %s)"
-    val = [(str(total_asset), str(adequacy_ratio), str(ROA), str(ROE), str(tot_shareholder_equity), str(company_id))]
+
+def insertDataBS(Total_Asset_Cash_Value, Total_SH_Equity_Value, Capital_Ratio, ROA, ROE, ROA_float, ROE_float, company_id):
+    sql = "INSERT INTO quarterly_dataBS (Total_Asset_Cash_Value, Total_SH_Equity_Value, Capital_Ratio, ROA, ROE, ROA_float, ROE_float, company_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    val = [(str(Total_Asset_Cash_Value), str(Total_SH_Equity_Value), str(Capital_Ratio), str(ROA), str(ROE), str(ROA_float), str(ROE_float), str(company_id))]
     mycursor.executemany(sql, val)
     mydb.commit()
-
-
-def revenues():
-    mycursor.execute("SELECT revenues FROM quarterly_dataPL")
-    myresult = mycursor.fetchall()
-    id = myresult[0]
-    print(id)
-    return id
